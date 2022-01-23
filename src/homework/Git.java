486151e5026c7f9be28902d4913aacdf71e77466
ahmetnyc <ahmet.arslan.nyc@gmail.com>
2022-01-22 23:16:04 -0500

    this is done from intellij directly

--- src/homework/Git.java
+++
@@ -1,7 +0,0 @@
-package homework;
-
-public class Git {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
---
+++ src/studyhall/RenasC4.java
@@ -0,0 +1,7 @@
+package studyhall;
+
+public class RenasC4 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
